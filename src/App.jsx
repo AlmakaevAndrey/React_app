fe53fa122ccc7b38af2046a78771cd4066d9c53c
AndreyAlmakaev <almakaevandrey@gmail.com>
2025-03-26 12:41:33 +0300

    not found page, questioncaed props, dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./components/MainLayout/MainLayout";
 import { HomePage } from "./pages/HomePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -8,9 +9,12 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
-          <Route path="/main" element={<div>main ! component !</div>} />
+          <Route path="/main" element={<div>! component !</div>} />
           <Route path="/addquestion" element={<div>!add question!s</div>} />
           <Route path="/forbidden" element={<div>forbidden !</div>} />
+          <Route path="/question/:id" element={<div>question page !</div>} />
+
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
