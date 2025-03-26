6ac7098995c6dfe46b43d063a1f7989f7527e1d7
AndreyAlmakaev <almakaevandrey@gmail.com>
2025-03-26 10:17:27 +0300

    MainLayout, routing, global

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,16 @@
-import {  } from 'react';
-import cls from "./Button.module.css"
+import {} from "react";
+import cls from "./Button.module.css";
 
 const isPrimary = true;
 
-export const Button = ({onClick, children}) => {
-    // console.log(onClick, children);
-    // console.log(cls);
-    return (
+export const Button = ({ onClick, children }) => {
+  // console.log(onClick, children);
+  // console.log(cls);
+  return (
     // <button className={isPrimary ? cls.primary : cls.btn}> Button </button>
-    <button className={`${cls.btn} ${isPrimary ? cls.primary : ""}`} 
-    onClick={onClick}
-    > {children} </button>
-    );
+    <button className={`${cls.btn} ${isPrimary ? cls.primary : ""}`} onClick={onClick}>
+      {" "}
+      {children}{" "}
+    </button>
+  );
 };
